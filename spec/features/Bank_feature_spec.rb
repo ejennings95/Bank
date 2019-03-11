0e46b005946ea71ff9d30069e1a5cd96b3d2b977
Elliot Jennings <elliot.r.jennings@outlook.com>
2019-03-11 14:20:40 +0000

    :construction: feature test for printing transaction

--- spec/features/Bank_feature_spec.rb
+++ spec/features/Bank_feature_spec.rb
@@ -1,9 +1,23 @@
 require_relative '../../lib/Account.rb'
 
 describe Account do
-  it 'should be possible to depsoit 100 pounds and then withdraw 50 pounds' do
-    account = Account.new
-    account.deposit(100)
-    expect(account.withdraw(50)).to eq "You have succesfully withdrawn £50. Your balance is now £50."
+
+  context "Account can deposit and withdraw" do
+
+    it 'should be possible to depsoit 100 pounds and then withdraw 50 pounds' do
+      account = Account.new
+      account.deposit(100)
+      expect(account.withdraw(50)).to eq "You have succesfully withdrawn £50. Your balance is now £50."
+    end
+  end
+
+  context "Printed statments" do
+    it 'should have the date of the transaction and final balance' do
+      account = Account.new
+      statement = Statment.new
+      date = Time.now.strftime(DD/MM/YY)
+      account.deposit(100)
+      expect(statment.print).to eq "#{date} || 100 || 0 || 100."
+    end
   end
 end
