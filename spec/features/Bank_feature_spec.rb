10cdee30bc650afd6fb2182ebf61624d010fce98
Elliot Jennings <elliot.r.jennings@outlook.com>
2019-03-11 14:07:45 +0000

    :truck: renaming feature spec file

--- spec/features/Bank_feature_spec.rb
+++ spec/features/Bank_feature_spec.rb
@@ -1,6 +1,9 @@
+require_relative '../../lib/Account.rb'
+
 describe Account do
   it 'should be possible to depsoit 100 pounds and then withdraw 50 pounds' do
     account = Account.new
     account.deposit(100)
     expect(account.withdrawl(50)).to eq "You have succesfully withdrawn £50."
   end
+end
