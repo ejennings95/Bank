a89b0fb73555da1768b3e828fb3726280b20cb5e
Elliot Jennings <elliot.r.jennings@outlook.com>
2019-03-11 17:07:40 +0000

    :construction: test for printing transactions through the printer class written

--- spec/Printer_spec.rb
+++ spec/Printer_spec.rb
@@ -8,4 +8,9 @@ describe Printer do
   it "should print the header: 'Date || Deposits || Withdrawls || Balance'" do
     expect(@printer.print_header).to eq "Date || Deposits || Withdrawls || Balance"
   end
+
+  it "should print the correct figures in the correct columns" do
+    transactions = [["11/09/14 || 100 || 0 || 100"]]
+    expect { @printer.print(transactions) }.to output("11/09/14 || 100 || 0 || 100\n").to_stdout
+  end
 end
