9dd359e47dce44890cb81d7aba2fa484cb3003b2
Elliot Jennings <elliot.r.jennings@outlook.com>
2019-03-11 14:50:57 +0000

    :sparkles: DateOfTransaction.format returns dd/mm/yy

---
+++ lib/DateOfTransaction.rb
@@ -0,0 +1,8 @@
+require 'Date'
+
+class DateOfTransaction
+
+  def format(date = Date.today)
+    date.strftime("%d/%m/%y")
+  end
+end
--- spec/DateOfTransaction_spec.rb
+++ spec/DateOfTransaction_spec.rb
@@ -4,6 +4,6 @@ describe DateOfTransaction do
   it "should return the date formatted dd/mm/yy" do
     date_of_transaction = DateOfTransaction.new
     date = Date.today
-    expect(date_of_transaction.format).to eq "#{date}.strftime(%d/%m/%y)"
+    expect(date_of_transaction.format).to eq "#{date.strftime('%d/%m/%y')}"
   end
 end
