e84b0b52a10adaaaba4e4328943ffac3f91be2cd
Elliot Jennings <elliot.r.jennings@outlook.com>
2019-03-11 14:09:05 +0000

    :white_check_mark: updated feature test to fit correct naming

--- spec/features/Bank_feature_spec.rb
+++ spec/features/Bank_feature_spec.rb
@@ -4,6 +4,6 @@ describe Account do
   it 'should be possible to depsoit 100 pounds and then withdraw 50 pounds' do
     account = Account.new
     account.deposit(100)
-    expect(account.withdrawl(50)).to eq "You have succesfully withdrawn £50."
+    expect(account.withdraw(50)).to eq "You have succesfully withdrawn £50. Your balance is now £50."
   end
 end
