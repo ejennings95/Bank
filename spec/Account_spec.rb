48a75ca7309359e107d8d827cd9cd361aa1538aa
Elliot Jennings <elliot.r.jennings@outlook.com>
2019-03-11 13:44:59 +0000

    ::white_check_mark: added a before each statement to Account_spec.rb

--- spec/Account_spec.rb
+++ spec/Account_spec.rb
@@ -1,8 +1,15 @@
 require_relative '../lib/Account.rb'
 
 describe Account do
+  before(:each) do
+    @account = Account.new
+  end
+
   it "should have an original balance of 0 pounds" do
-    account = Account.new
-    expect(account.balance).to eq 0
+    expect(@account.balance).to eq 0
+  end
+
+  it "should be possible to deposit money to your account" do
+    expect(@account.deposit(10)).to eq "You have succesfully deposited £10."
   end
 end
