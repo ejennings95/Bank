71853fd2b66fb263910433b44cce443a0263a9df
Elliot Jennings <elliot.r.jennings@outlook.com>
2019-03-12 12:15:20 +0000

    :sparkles: amount and balance returned to two decimal places

--- lib/Account.rb
+++ lib/Account.rb
@@ -10,20 +10,23 @@ class Account
 
   def deposit(amount)
     @balance += amount
-    @statement.new_transaction(amount, 0, @balance)
-    "You have successfully deposited £#{amount}. Your balance is now £#{@balance}."
+    @statement.new_transaction(two_decimal_places(amount), 0, two_decimal_places(@balance))
+    "You have successfully deposited £#{two_decimal_places(amount)}. Your balance is now £#{two_decimal_places(@balance)}."
   end
 
   def withdraw(amount)
     sufficient_funds?(amount)
     @balance -= amount
-    @statement.new_transaction(0, amount, @balance)
-    "You have successfully withdrawn £#{amount}. Your balance is now £#{@balance}."
+    @statement.new_transaction(0, two_decimal_places(amount), two_decimal_places(@balance))
+    "You have successfully withdrawn £#{two_decimal_places(amount)}. Your balance is now £#{two_decimal_places(@balance)}."
   end
 
   private
-
   def sufficient_funds?(amount)
     @balance > amount ? true : (raise 'Insufficient funds.')
+  end
+
+  def two_decimal_places(amount)
+    '%.2f' % amount
   end
 end
