bc7a6c616b917f18775000bc8da1d078cc2dbee2
Elliot Jennings <elliot.r.jennings@outlook.com>
2019-03-12 12:39:04 +0000

    :bug: amount is only added and subtracted from balance to2 dp

--- lib/Account.rb
+++ lib/Account.rb
@@ -9,21 +9,22 @@ class Account
   end
 
   def deposit(amount)
-    @balance += amount
+    @balance += amount.round(2)
     @statement.new_transaction(two_decimal_places(amount), 0, two_decimal_places(@balance))
     "You have successfully deposited £#{two_decimal_places(amount)}. Your balance is now £#{two_decimal_places(@balance)}."
   end
 
   def withdraw(amount)
     sufficient_funds?(amount)
-    @balance -= amount
+    @balance -= amount.round(2)
     @statement.new_transaction(0, two_decimal_places(amount), two_decimal_places(@balance))
     "You have successfully withdrawn £#{two_decimal_places(amount)}. Your balance is now £#{two_decimal_places(@balance)}."
   end
 
   private
+
   def sufficient_funds?(amount)
-    @balance > amount ? true : (raise 'Insufficient funds.')
+    @balance > amount.round(2) ? true : (raise 'Insufficient funds.')
   end
 
   def two_decimal_places(amount)
