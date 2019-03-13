fc556ab3ae3b81e93d079ebb3f91dbf57023202f
Elliot Jennings <elliot.r.jennings@outlook.com>
2019-03-13 10:44:42 +0000

    :white_check_mark::clown_face: update Date of Transaction test to include double

--- spec/date_of_transaction_spec.rb
+++ spec/date_of_transaction_spec.rb
@@ -1,9 +1,10 @@
 require_relative '../lib/date_of_transaction.rb'
 
 describe DateOfTransaction do
+  let(:testdate) { '2019-03-13' }
+
   it 'should return the date formatted dd/mm/yy' do
     date_of_transaction = DateOfTransaction.new
-    date = Date.today
-    expect(date_of_transaction.format).to eq date.strftime('%d/%m/%y').to_s
+    expect(date_of_transaction.format).to eq '13/03/19'
   end
 end
